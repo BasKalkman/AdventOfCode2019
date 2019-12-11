ccb7dff2bfdd669c5e7e0fb9a1a56fe22df39a45
Bas K <39069678+Basje12@users.noreply.github.com>
2019-12-11 18:00:31 +0100

    Day 7, WiP

--- Day-7/solution.js
+++ Day-7/solution.js
@@ -5,64 +5,49 @@ const data = fs
     .split(',')
     .map(Number);
 
-function getInputArray(i) {
+// Generate phaseSettings array
+let phaseSettings = [];
+for (let i = 0; i < Math.pow(5, 5); i++) {
     let n1 = i % 5;
     let n2 = Math.floor(i / 5) % 5;
     let n3 = Math.floor(i / 25) % 5;
     let n4 = Math.floor(i / 125) % 5;
     let n5 = Math.floor(i / 625) % 5;
 
-    return `${n5}${n4}${n3}${n2}${n1}`.split('').map(Number);
+    let arr = [n5, n4, n3, n2, n1];
+    let test = [...new Set(arr)];
+
+    if (test.length == arr.length) {
+        phaseSettings.push(arr);
+    }
 }
 
+// Generate amplifiers
 const amplifiers = [];
 for (let i = 0; i < 5; i++) {
-    let pc = new IntCodeComputer(data);
+    let pc = new IntCodeComputer(data.slice(0));
     amplifiers.push(pc);
 }
 
 const outputs = [];
-let highestOutput = 0;
-for (let i = 0; i < Math.pow(5, 5); i++) {
-    // Get next phase setting
-    let phaseSetting = getInputArray(i);
-    // Reset amps
-    amplifiers.forEach(e => e.reset());
+for (let i = 0; i < phaseSettings.length; i++) {
+    let phaseSetting = phaseSettings[i];
 
-    // Run amp 1
-    amplifiers[0].changeInput(phaseSetting[0], 0);
-    let resultAmp1 = amplifiers[0].processCode();
+    let result = 0;
+    // Reset computers, set phase
+    amplifiers.forEach((e, index) => {
+        e.reset();
+        e.setData(data.slice(0));
+        e.setPhase(phaseSetting[index]);
+        e.setInput(result);
+        result = e.processCode();
+    });
 
-    // Run amp 2
-    amplifiers[1].changeInput(phaseSetting[1], resultAmp1);
-    let resultAmp2 = amplifiers[1].processCode();
-
-    // Run amp 3
-    amplifiers[2].changeInput(phaseSetting[2], resultAmp2);
-    let resultAmp3 = amplifiers[2].processCode();
-
-    // Run amp 4
-    amplifiers[3].changeInput(phaseSetting[3], resultAmp3);
-    let resultAmp4 = amplifiers[3].processCode();
-
-    // Run amp 5
-    amplifiers[4].changeInput(phaseSetting[4], resultAmp4);
-    let resultAmp5 = amplifiers[4].processCode();
-
-    outputs.push({ resultAmp5, phaseSetting });
-    if (resultAmp5 > highestOutput) {
-        highestOutput = resultAmp5;
-    }
+    outputs.push({ result, phaseSetting });
 }
 
-// let highestOutput = 0;
-// let phaseTest = [];
-// for (let output of outputs) {
-//     if (output.resultAmp5 > highestOutput) {
-//         highestOutput = output.resultAmp5;
-//         phaseTest = output.phaseSetting;
-//     }
-// }
+let highest = outputs.reduce((a, c) => {
+    return a.result > c.result ? a : c;
+}, {});
 
-// console.log('Highest output: ', Math.max(...outputs));
-console.log('Highest output: ', highestOutput);
+console.log('highest: ', highest);
